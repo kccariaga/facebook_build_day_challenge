488ac079083a5822a2eb48d681e32eaec087e60c
kccariaga <kccariag@ucsc.edu>
2018-07-16 00:10:36 -0700

    Documentation explaining the purpose of new js files added in App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 import Main from './Main';
 
+/*
+ *  These are the components of the website:
+ *      Main.js         **Controls the Routes for each webpage, 
+ *                         create new path here  when creating a new page**
+ * _________________________________________________________________________________
+ * 
+ *      Home.js         contains the formatting for the (./) home Page
+ *      Workshop.js     contains the formatting for ./workshops page
+ *      Projects.js     contains the formatting for ./projects page
+ *  Edit the components in the corresponding javascript files
+ * 
+ *  -KRON
+ */
+
 class App extends Component {
   render() {
     return (
@@ -11,9 +25,13 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Let's Code Together</h1>
         </header>
-        
         <p className="App-intro">
           <div>
+            {/* TODO: Add Code for Nav Bar Below */}
+
+
+
+            {/* This loads the contents of Main.js, which controls the routes to other webpages */}
              <Main /> 
           </div>
         </p>
--- src/Workshops.js
+++ src/Workshops.js
@@ -1,12 +1,14 @@
 // This is where the html code for the /workshops page of the
 // website will go.
 
+// TODO:
+
 import React from 'react'
 
 
 const Workshops = () => (
     <p>
-        MeowMeowMeow
+        This is the empty Workshops Page
     </p>
 
 )
